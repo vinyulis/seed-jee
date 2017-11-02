0bc52a76c4e26b7e344932428d403f87c5895a59
Alfons Serra <alfonsoserra@gmail.com>
2017-11-02 16:31:31 +0100

    Change user table field

--- src/main/resources/META-INF/load.sql
+++ src/main/resources/META-INF/load.sql
@@ -1,2 +1,2 @@
-INSERT INTO SeedUser (id, login, firstName, lastName, password, userrole) values (1, 'quentinada', 'Quentin', 'ADAM', 'Lm4PZdOzxeuHrUKU26YB4GnwBFViydvjnrOAZH7BhaA=','ADMIN');
+INSERT INTO SeedUser (id, login, name, surname, password, userrole) values (1, 'quentinada', 'Quentin', 'ADAM', 'Lm4PZdOzxeuHrUKU26YB4GnwBFViydvjnrOAZH7BhaA=','ADMIN');
 
