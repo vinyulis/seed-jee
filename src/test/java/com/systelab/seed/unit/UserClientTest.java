c2df28e51d3879528a8aa1ba4c3ab48fcaffd7a4
alfonsserra <alfonsoserra@gmail.com>
2018-03-09 18:14:42 +0100

    Add Test for user creation

--- src/test/java/com/systelab/seed/unit/UserClientTest.java
+++ src/test/java/com/systelab/seed/unit/UserClientTest.java
@@ -1,5 +1,6 @@
 package com.systelab.seed.unit;
 
+import com.systelab.seed.TestUtil;
 import com.systelab.seed.client.RequestException;
 import com.systelab.seed.client.UserClient;
 import com.systelab.seed.model.user.User;
@@ -7,6 +8,7 @@ import com.systelab.seed.model.user.User;
 import java.util.List;
 import java.util.logging.Logger;
 
+import com.systelab.seed.model.user.UserRole;
 import org.junit.jupiter.api.*;
 import io.qameta.allure.*;
 
@@ -43,4 +45,24 @@ public class UserClientTest extends BaseClientTest {
         Assertions.assertNotNull(users);
     }
 
+    @TmsLink("SEED-USR-2")
+    @Link(value = "REQ-USR-2", type = "requirement")
+    @DisplayName("Test create a User.")
+    @Description("Test that is possible to create a user.\n\nPrerequisites:\n\n" + "- Prerequisite 1\n" + "- Prerequisite 2\n" + "- Prerequisite 3\n")
+    @Tag("user")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testCreateUser() throws RequestException {
+        User user =new User();
+        user.setLogin("agoncalves");
+        user.setPassword("agoncalves");
+        user.setName("Antonio");
+        user.setSurname("Goncalves");
+        user.setRole(UserRole.ADMIN);
+
+        User userCreated = clientForUser.create(user);
+        TestUtil.checkObjectIsNotNull("user", userCreated);
+        TestUtil.checkField("Name", "Antonio", userCreated.getName());
+        TestUtil.checkField("Surname", "Goncalves", userCreated.getSurname());
+    }
 }
